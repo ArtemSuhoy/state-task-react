d85b5d6723f9d8f086ea257d3ee3d2b43c0e0c7b
Artem Suhoy <artemsuhoy7@gmail.com>
2021-11-10 14:32:23 +0200

    Task7

--- state-task-react/src/App.js
+++ state-task-react/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css"
-
+import { Task7 } from "./task7"
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Task7 name="Dropdown" />
+    </div>
+  )
 }
 
 export default App
